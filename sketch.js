bd47ef0c0aef81a63e50ca7ff810fa15fc3249ed
Abdullah AlAttar <abdullah.alattar@cis.asu.edu.eg>
2017-05-04 13:56:58 +0200

    another change

--- sketch.js
+++ sketch.js
@@ -8,4 +8,5 @@ function draw() {
   ellipse(100,100,50,70,30);
   arc(50, 50, 80, 80, 0, PI+QUARTER_PI, OPEN)
    arc(50, 55, 80, 80, 0, PI+QUARTER_PI, OPEN)
+   arc(55, 55, 80, 80, 0, PI+QUARTER_PI, OPEN)
 } 
