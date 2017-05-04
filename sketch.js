f27fc164d3b45817fef6403dba050d1e1d300aba
Abdullah AlAttar <abdullah.alattar@cis.asu.edu.eg>
2017-05-04 14:06:50 +0200

    another one .

--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ function setup() {
 }
 
 function draw() {
-  background(255,0,0);
-  ellipse(300,300,50,50);
-  ellipse(100,100,50,70,30);
-  arc(50, 50, 80, 80, 0, PI+QUARTER_PI, OPEN)
-   arc(50, 55, 80, 80, 0, PI+QUARTER_PI, OPEN)
-   arc(55, 55, 80, 80, 0, PI+QUARTER_PI, OPEN)
+  background(255, 0, 0);
+  ellipse(300, 300, 50, 50);
+  ellipse(100, 100, 50, 70, 30);
+  arc(50, 50, 80, 80, 0, PI + QUARTER_PI, OPEN)
+  arc(50, 55, 80, 80, 0, PI + QUARTER_PI, OPEN)
+  arc(55, 55, 80, 80, 0, PI + QUARTER_PI, OPEN)
+  arc(55, 80, 80, 80, 0, PI + QUARTER_PI, OPEN)
 } 
