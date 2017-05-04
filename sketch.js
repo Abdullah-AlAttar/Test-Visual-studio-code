53db90bcca1e62be3fa36a05977a3b35bf0c596b
Abdullah AlAttar <abdullah.alattar@cis.asu.edu.eg>
2017-05-04 13:51:51 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+  background(255,0,0);
+  ellipse(300,300,50,50);
+  ellipse(100,100,50,70,30);
+  arc(50, 50, 80, 80, 0, PI+QUARTER_PI, OPEN)
+} 
